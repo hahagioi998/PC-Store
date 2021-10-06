8a5754abd51d8cba4a9e38c5e8115095d9309fba
IvanAndreski <63723286+IvanAndreski@users.noreply.github.com>
2021-10-06 16:52:45 +0200

    installed bootstrap and put simple navbar

---
+++ pc-store/src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>HomePage</h1>
+        <h3>Grid</h3>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
